574a77dabe96fed3f698b0026958a1c1c2e1ea27
TaurusXin <i@taurusxin.com>
2024-10-21 16:29:17 +0800

    feat: rename binary to ncmdump-go

--- build.sh
+++ build.sh
@@ -8,30 +8,30 @@ mkdir build
 
 # Linux amd64
 echo "Building for Linux amd64..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/ncmdump github.com/taurusxin/ncmdump-go
-tar zcf build/ncmdump_linux_amd64_$VERSION.tar.gz -C build ncmdump
-rm build/ncmdump
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o build/ncmdump-go github.com/taurusxin/ncmdump-go
+tar zcf build/ncmdump-go_linux_amd64_$VERSION.tar.gz -C build ncmdump-go
+rm build/ncmdump-go
 
 # Linux arm64
 echo "Building for Linux arm64..."
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o build/ncmdump github.com/taurusxin/ncmdump-go
-tar zcf build/ncmdump_linux_arm64_$VERSION.tar.gz -C build ncmdump
-rm build/ncmdump
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-w -s" -o build/ncmdump-go github.com/taurusxin/ncmdump-go
+tar zcf build/ncmdump-go_linux_arm64_$VERSION.tar.gz -C build ncmdump-go
+rm build/ncmdump-go
 
 # macOS amd64
 echo "Building for macOS amd64..."
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o build/ncmdump github.com/taurusxin/ncmdump-go
-tar zcf build/ncmdump_darwin_amd64_$VERSION.tar.gz -C build ncmdump
-rm build/ncmdump
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-w -s" -o build/ncmdump-go github.com/taurusxin/ncmdump-go
+tar zcf build/ncmdump-go_darwin_amd64_$VERSION.tar.gz -C build ncmdump-go
+rm build/ncmdump-go
 
 # macOS arm64
 echo "Building for macOS arm64..."
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-w -s" -o build/ncmdump github.com/taurusxin/ncmdump-go
-tar zcf build/ncmdump_darwin_arm64_$VERSION.tar.gz -C build ncmdump
-rm build/ncmdump
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-w -s" -o build/ncmdump-go github.com/taurusxin/ncmdump-go
+tar zcf build/ncmdump-go_darwin_arm64_$VERSION.tar.gz -C build ncmdump-go
+rm build/ncmdump-go
 
 # Windows amd64
 echo "Building for Windows amd64..."
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o build/ncmdump.exe github.com/taurusxin/ncmdump-go
-zip -q -j build/ncmdump_windows_amd64_$VERSION.zip ./build/ncmdump.exe
-rm build/ncmdump.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-w -s" -o build/ncmdump-go.exe github.com/taurusxin/ncmdump-go
+zip -q -j build/ncmdump-go_windows_amd64_$VERSION.zip ./build/ncmdump-go.exe
+rm build/ncmdump-go.exe
--- go.mod
+++ go.mod
@@ -10,8 +10,8 @@ require github.com/bogem/id3v2/v2 v2.1.4
 
 require (
 	github.com/TwiN/go-color v1.4.1
-	github.com/go-flac/flacpicture/v2 v2.0.2
-	github.com/go-flac/go-flac/v2 v2.0.1
+	github.com/go-flac/flacpicture v0.3.0
+	github.com/go-flac/go-flac v1.0.0
 )
 
 require (
