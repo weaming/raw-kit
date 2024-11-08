88314554d0228a71cd2ea6b7bddaa991f015662e
TaurusXin <i@taurusxin.com>
2024-11-08 15:01:14 +0800

    release 1.7.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=1.7.0
+VERSION=1.7.1
 
 # Clean up the build directory
 rm -rf build
--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func main() {
 	}
 
 	if *showVersion {
-		fmt.Println("ncmdump version 1.7.0")
+		fmt.Println("ncmdump version 1.7.1")
 		os.Exit(0)
 	}
 
